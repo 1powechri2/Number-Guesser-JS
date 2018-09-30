2739c407a67639f84cbbd4d5d3dc90187f6dc0fa
Chris Powell <chris.ian.powell@gmail.com>
2018-09-30 15:14:40 -0600

    fixes bugs found while creating annotated branch

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var number = 0
 var counter = 0;
+var min_numb = 0;
+var max_numb = 100;
 
 document.getElementById('clear_button').disabled = true;
 document.getElementById('clear_button').style.backgroundColor = '#D0D2D3'
@@ -7,20 +9,20 @@ document.getElementById('reset_button').disabled = true;
 document.getElementById('reset_button').style.backgroundColor = '#D0D2D3'
 
 document.getElementById('range_button').onclick = function(){
-  min = document.getElementById('min_number').value;
-  max = document.getElementById('max_number').value;
+  min_numb = document.getElementById('min_number').value;
+  max_numb = document.getElementById('max_number').value;
 
-  min = parseInt(min)
-  max = parseInt(max)
+  min_numb = parseInt(min_numb)
+  max_numb = parseInt(max_numb)
 
-  number = Math.floor(Math.random() * (max - min) + min);
+  number = Math.floor(Math.random() * (max_numb - min_numb) + min_numb);
 
-  if (min > max) {
+  if (min_numb > max_numb) {
     document.getElementById('answer-1').innerHTML = 'The Minimum Value Is';
     document.getElementById('answer-2').innerHTML = 'Higher Than The Maximum';
     document.getElementById('answer-3').innerHTML = 'Please Input New Values';
     document.getElementById('answer-4').innerHTML = 'For Minimum and Maximum';
-  } else if (isNaN(min) || isNaN(max)){
+  } else if (isNaN(min_numb) || isNaN(max_numb)){
     document.getElementById('answer-1').innerHTML = 'One Of The Values You Have';
     document.getElementById('answer-2').innerHTML = 'Entered Was Not A Number.';
     document.getElementById('answer-3').innerHTML = 'Please Enter New Minimum';
@@ -49,10 +51,12 @@ document.getElementById('number_field').onkeyup = function(){
 document.getElementById('clear_number').onclick = function(){
   document.getElementById('number_field').value = ''
   document.getElementById('clear_button').disabled = true;
+  document.getElementById('clear_button').style.backgroundColor = '#D0D2D3'
 };
 
 document.getElementById('guess_number').onclick = function(){
   document.getElementById('clear_button').disabled = true;
+  document.getElementById('clear_button').style.backgroundColor = '#D0D2D3';
   counter++;
 
   var guess = document.getElementById('number_field').value;
@@ -66,25 +70,33 @@ document.getElementById('guess_number').onclick = function(){
     document.getElementById('answer-3').innerHTML = ''
     document.getElementById('answer-4').innerHTML = ''
   }
-  else if (guessedNumber > max) {
+  else if (guessedNumber > max_numb) {
     document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
+    document.getElementById('answer-2').innerHTML = ''
     document.getElementById('answer-num').innerHTML = `${guessedNumber}`
     document.getElementById('answer-3').innerHTML = 'Which Is Higher than the Possible Maximum Value'
+    document.getElementById('answer-4').innerHTML = ''
   }
-  else if (guessedNumber < min) {
+  else if (guessedNumber < min_numb) {
     document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
+    document.getElementById('answer-2').innerHTML = ''
     document.getElementById('answer-num').innerHTML = `${guessedNumber}`
     document.getElementById('answer-3').innerHTML = 'Which Is Lower than the Possible Maximum Value'
+    document.getElementById('answer-4').innerHTML = ''
   }
   else if (guessedNumber > number) {
     document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
+    document.getElementById('answer-2').innerHTML = ''
     document.getElementById('answer-num').innerHTML = `${guessedNumber}`
     document.getElementById('answer-3').innerHTML = 'Which is too High'
+    document.getElementById('answer-4').innerHTML = ''
   }
   else if (guessedNumber < number) {
     document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
+    document.getElementById('answer-2').innerHTML = ''
     document.getElementById('answer-num').innerHTML = `${guessedNumber}`
     document.getElementById('answer-3').innerHTML = 'Which is too low'
+    document.getElementById('answer-4').innerHTML = ''
   }
   else {
     document.getElementById('answer-1').innerHTML = 'BOOM!'
@@ -92,9 +104,9 @@ document.getElementById('guess_number').onclick = function(){
     document.getElementById('answer-2').innerHTML = `You Got The Answer in ${counter} Guesses`
     document.getElementById('answer-3').innerHTML = 'A New Possible Answer is Waiting to be Discovered'
     document.getElementById('answer-4').innerHTML = 'The Max Has Been Increased By Ten And The Min has been decreased by Ten'
-    max = parseInt(max) + 10
-    min = parseInt(min) - 10
-    number = Math.floor(Math.random() * (max - min) + min);
+    max_numb = max_numb + 10
+    min_numb = min_numb - 10
+    number = Math.floor(Math.random() * (max_numb - min_numb) + min_numb);
     counter = 0
   }
   document.getElementById('number_field').value = ''
